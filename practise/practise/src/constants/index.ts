9946d9b69c61f30e60c2ab5fcc49ccb978d3c9ce
caokaixiang <alexcao911@outlook.com>
2025-10-14 19:14:14 +0800

    feat: NavBar

---
+++ practise/practise/src/constants/index.ts
@@ -0,0 +1,9 @@
+export const navLinks = [
+    { label: 'Store'},
+    { label: 'Mac'},
+    { label: 'iPhone'},
+    { label: 'iPad'},
+    { label: 'Watch'},
+    { label: 'Vision'},
+    { label: 'AirPods'},
+]
