1e523173ee3bd699652a6a6f019507d312389133
caokaixiang <alexcao911@outlook.com>
2025-10-16 21:37:20 +0800

    fix:naming

--- practise/practise/src/App.tsx
+++ practise/practise/src/App.tsx
@@ -6,8 +6,8 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Showcase from './components/Showcase';
 import Performance from'./components/Performance';
 import Features from'./components/Features';
-import Highlights from './components/Highlights ';
-import Footer from './components/Footer'
+import Highlights from './components/Highlights';
+import Footer from './components/Footer';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);;
